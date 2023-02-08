285e77cb877fcb368a341ccddc304600b33de13f
Tufan Çağkan Sökmen <>
2023-02-08 03:05:21 +0300

    Force ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            \URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', 'on');
+        }
+
     }
 }
